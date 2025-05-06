785be489f9eca2ff00734ef575c4d9835c29d222
Illya Soloviov <illyalesnik@gmail.com>
2025-05-06 13:07:00 +0300

    new tests gitActions

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 const { romanNumbers } = require('./math.js');
 
-console.log(romanNumbers(1) === 'I', 'Test 1 failed');
-console.log(romanNumbers(3999) === 'MMMCMXCIX', 'Test 3999 failed');
-console.log(romanNumbers(44) === 'XLIV', 'Test 44 failed');
-console.log(romanNumbers(2023) === 'MMXXIII', 'Test 2023 failed');
+console.assert(romanNumbers(1) === 'I', 'Test 1 failed');
+console.assert(romanNumbers(3999) === 'MMMCMXCIX', 'Test 3999 failed');
+console.assert(romanNumbers(44) === 'XLIV', 'Test 44 failed');
+console.assert(romanNumbers(2023) === 'MMXXIII', 'Test 2023 failed');
 
-console.log(romanNumbers(4000) === 'Не корректні данні! Максимально допустиме число "3999"', 'Test 4000 failed');
-console.log(romanNumbers("abc") === 'Не корректні данні!', 'Test "abc" failed');
+console.assert(romanNumbers(4000) === 'Не корректні данні! Максимально допустиме число "3999"', 'Test 4000 failed');
+console.assert(romanNumbers("abc") === 'Не корректні данні! Максимально допустиме число "3999"', 'Test "abc" failed');
 
 console.log("✅ All tests passed!");
+
